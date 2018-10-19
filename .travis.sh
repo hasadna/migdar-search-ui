80da8673a9f9d46d58871c34709b9c139d57208d
Ori Hoch <ori@uumpa.com>
2018-10-19 15:11:04 +0300

    travis: set correct docker image

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
+DOCKER_IMAGE=uumpa/hasadna-migdar-internal-search-ui
+REACT_APP_API_URL=https://migdar-internal-search-backend.odata.org.il/
+
+
 if [ "${1}" == "script" ]; then
-    docker build -t uumpa/migdar-internal-search-ui:latest . && exit 0
+    docker build -t ${DOCKER_IMAGE}:latest . && exit 0
 elif [ "${1}" == "deploy" ]; then
-    docker push uumpa/migdar-internal-search-ui:latest &&\
-    docker build --build-arg REACT_APP_API_URL=https://migdar-internal-search-backend.odata.org.il/ \
-           -t uumpa/migdar-internal-search-ui:latest-prod -t uumpa/migdar-internal-search-ui:${TRAVIS_COMMIT} . &&\
-    docker push uumpa/migdar-internal-search-ui:latest-prod && docker push uumpa/migdar-internal-search-ui:${TRAVIS_COMMIT} &&\
+    docker push ${DOCKER_IMAGE}:latest &&\
+    docker build --build-arg REACT_APP_API_URL=${REACT_APP_API_URL} \
+           -t ${DOCKER_IMAGE}:latest-prod -t ${DOCKER_IMAGE}:${TRAVIS_COMMIT} . &&\
+    docker push ${DOCKER_IMAGE}:latest-prod && docker push ${DOCKER_IMAGE}:${TRAVIS_COMMIT} &&\
     exit 0
 fi; exit 1
